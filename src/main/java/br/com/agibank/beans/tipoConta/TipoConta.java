112a229fda6d428f90b3513c2d07f06d15897857
unknown <bruno.henrique@NB-5LH3864.AGIPLAN.LOCAL>
2025-03-18 08:29:56 -0300

    :sparkles: feat(ContaInterna, TipoConta) Adiciona metodo criar conta interna, e criação da classa TipoConta

---
+++ src/main/java/br/com/agibank/beans/tipoConta/TipoConta.java
@@ -0,0 +1,28 @@
+package br.com.agibank.beans.tipoConta;
+
+public class TipoConta {
+
+    private int idTipoConta;
+    private String tipo;
+
+    public TipoConta(int idTipoConta, String tipo) {
+        this.idTipoConta = idTipoConta;
+        this.tipo = tipo;
+    }
+
+    public int getIdTipoConta() {
+        return idTipoConta;
+    }
+
+    public void setIdTipoConta(int idTipoConta) {
+        this.idTipoConta = idTipoConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/br/com/agibank/dao/tipoConta/TipoContaDAO.java
@@ -0,0 +1,4 @@
+package br.com.agibank.dao.tipoConta;
+
+public class TipoContaDAO {
+}
