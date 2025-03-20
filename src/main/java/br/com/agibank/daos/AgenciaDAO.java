70c75528d46af662693c9db713e615fb12e2663b
jennifer.parreira <jennifer.parreira2017@gmail.com>
2025-03-20 11:41:34 -0300

    :recycle: Atualiza as classes Suporte e Agencia

--- src/main/java/br/com/agibank/daos/AgenciaDAO.java
+++ src/main/java/br/com/agibank/daos/AgenciaDAO.java
@@ -11,99 +11,90 @@ import java.sql.SQLException;
 
 public class AgenciaDAO {
 
-    public void inserirAgencia(String rua, int numero, String complemento, String cidade) {
+    private Connection con;
+    private PreparedStatement stmt;
+    private ResultSet rs;
+
+
+    public AgenciaDAO() throws SQLException {
+        con = Conexao.getConnection();
+    }
+
+    public void fecharConexao() throws SQLException {
+        con.close();
+    }
+
+    public void inserirAgencia(String rua, int numero, String complemento, String cidade) throws SQLException{
         String sql = "INSERT INTO Agencia (rua,numero,complemento) VALUES (?,?,?,?)";
 
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        PreparedStatement stmt = con.prepareStatement(sql);
 
-            stmt.setString(1, rua);
-            stmt.setInt(2, numero);
-            stmt.setString(3, complemento);
-            stmt.setString(4, cidade);
-            stmt.executeUpdate();
-            System.out.println("Agencia inserida com sucesso");
+        stmt.setString(1, rua);
+        stmt.setInt(2, numero);
+        stmt.setString(3, complemento);
+        stmt.setString(4, cidade);
+        stmt.executeUpdate();
+        System.out.println("Agencia inserida com sucesso");
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
-    public void deletarAgencia(int id_agencia) {
+    public void deletarAgencia(int id_agencia) throws SQLException{
         String sql = "DELETE FROM Agencia WHERE id_agencia = ?";
 
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        PreparedStatement stmt = con.prepareStatement(sql);
 
-            stmt.setInt(1, id_agencia);
-            stmt.executeUpdate();
-            System.out.println("Teste realizado com sucesso");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        stmt.setInt(1, id_agencia);
+        stmt.executeUpdate();
+        System.out.println("Teste realizado com sucesso");
     }
 
-    public void alterarEndereco(int id_agencia, String rua, int numero, String complemento, String cidade){
+    public void alterarEndereco(int id_agencia, String rua, int numero, String complemento, String cidade) throws SQLException{
         String sql = "UPDATE Agencia SET rua = ?, numero = ?, complemento = ?, cidade = ? WHERE id_agencia = ?";
 
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+        PreparedStatement stmt = con.prepareStatement(sql);
 
-            stmt.setString(1, rua);
-            stmt.setInt(2, numero);
-            stmt.setString(3, complemento);
-            stmt.setString(4, cidade);
-            stmt.setInt(5,id_agencia);
-            stmt.executeUpdate();
-            System.out.println("Endereço atualizado com sucesso");
+        stmt.setString(1, rua);
+        stmt.setInt(2, numero);
+        stmt.setString(3, complemento);
+        stmt.setString(4, cidade);
+        stmt.setInt(5,id_agencia);
+        stmt.executeUpdate();
+        System.out.println("Endereço atualizado com sucesso");
+    }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public void listarAgencias() throws SQLException{
+        String sql = "SELECT * FROM Agencia";
+
+        PreparedStatement stmt = con.prepareStatement(sql);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            System.out.print(rs.getInt("id_agencia") + " ");
+            System.out.print(rs.getString("rua") + " ");
+            System.out.print(rs.getString("numero") + " ");
+            System.out.print(rs.getString("complemento") + " ");
+            System.out.println(rs.getString("cidade") + "\n");
         }
     }
 
-    public void listarAgencias(){
-            String sql = "SELECT * FROM Agencia";
-
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                System.out.print(rs.getInt("id_agencia") + " ");
-                System.out.print(rs.getString("rua") + " ");
-                System.out.print(rs.getString("numero") + " ");
-                System.out.print(rs.getString("complemento" + " "));
-                System.out.println(rs.getString("cidade"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Agencia buscarAgenciaPorId(int id_agencia){
+    public Agencia buscarAgenciaPorId(int id_agencia) throws SQLException{
         String sql = "SELECT * FROM Agencia WHERE id_agencia = ?";
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            stmt.setInt(1, id_agencia);
-            ResultSet rs = stmt.executeQuery();
+        PreparedStatement stmt = con.prepareStatement(sql);
 
-            if (rs.next()) {
-                Agencia agencia = new Agencia();
-                agencia.setComplemento(rs.getString("complemento"));
-                agencia.setNumero(rs.getInt("numero"));
-                agencia.setRua(rs.getString("rua"));
-                agencia.setCidade(rs.getString("cidade"));
-                return agencia;
-            }
+        stmt.setInt(1, id_agencia);
+        ResultSet rs = stmt.executeQuery();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (rs.next()) {
+            Agencia agencia = new Agencia();
+            agencia.setComplemento(rs.getString("complemento"));
+            agencia.setNumero(rs.getInt("numero"));
+            agencia.setRua(rs.getString("rua"));
+            agencia.setCidade(rs.getString("cidade"));
+            return agencia;
         }
+
         return null;
     }
 }
