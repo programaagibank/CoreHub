18c0e1c260a08a96b0ee794f9a266ec7c45297d8
Mizael <Mizaelmiguel246@gmail.com>
2025-03-22 08:37:23 -0300

    :sparkles: feat(taxa) Funcionalidade CRUD de taxa

--- src/main/java/br/com/agibank/Main.java
+++ src/main/java/br/com/agibank/Main.java
@@ -1,6 +1,33 @@
 package br.com.agibank;
 
+import br.com.agibank.beans.taxa.Taxa;
+import br.com.agibank.dao.taxa.TaxaDAO;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            TaxaDAO taxaDAO = new TaxaDAO();
+            int resultadoCadastro = taxaDAO.cadastrarTaxa(1, "teste2", 9.9);
+            System.out.println("Cadastro realizado? " + (resultadoCadastro > 0));
+
+            String taxaEncontrada = taxaDAO.buscarTaxa(1);
+            System.out.println(taxaEncontrada);
+
+            Taxa taxaAtualizada = new Taxa();
+            taxaAtualizada.setId(1); // Define o ID da taxa a ser atualizada
+            int resultadoAtualizacao = taxaDAO.atualizarTaxa(taxaAtualizada);
+            System.out.println("Atualizacao realizada? " + (resultadoAtualizacao > 0));
+
+            int resultadoExclusao = taxaDAO.excluirTaxa(1);
+            System.out.println("Exclusao realizada? " + (resultadoExclusao > 0));
+
+            taxaDAO.fecharConexao();
+        } catch (SQLException e) {
+            System.err.println("ERRO AO EXECUTAR OPERAÇÃO NO BANCO DE DADOS: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
 }
